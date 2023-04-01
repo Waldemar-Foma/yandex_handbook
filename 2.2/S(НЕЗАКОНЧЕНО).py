9ff6fa1c488bd3cb4b96ac55238c69484191a7b8
Foma <104798936+FomaVoma514@users.noreply.github.com>
2023-04-01 03:31:24 +0200

    Update S(НЕЗАКОНЧЕНО).py

--- 2.2/S(НЕЗАКОНЧЕНО).py
+++ 2.2/S(НЕЗАКОНЧЕНО).py
@@ -10,19 +10,3 @@
 Если таких строк несколько, выбрать ту, что меньше всех лексикографически.
 '''
 
-first_side, second_side, third_side = input(), input(), input()
-find_word = 'зайка'
-length = len(first_side)
-find_side = ''
-if find_word in first_side:
-    find_side = first_side
-    length = len(first_side)
-if find_word in second_side:
-    if find_side >= second_side or find_side == '':
-        length = len(second_side)
-        find_side = second_side
-if find_word in third_side:
-    if find_side >= third_side or find_side == '':
-        length = len(third_side)
-        find_side = third_side
-print(f"{find_side} {length}")
